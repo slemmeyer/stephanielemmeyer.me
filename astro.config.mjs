15c33ad40e78205cff873ce1117dfd234e9489da
Stephanie Lemmeyer <stephanie.lemmeyer@gmail.com>
2025-02-20 10:54:46 -0500

    added github pages deployment support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://stephanielemmeyer.me',
   integrations: [
     tailwind()
   ]
