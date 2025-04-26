0a49dd16b55a15e675ae523e4faba65fae9429c4
Stephanie Lemmeyer <stephanie.lemmeyer@gmail.com>
2025-04-26 02:15:19 -0400

    fixing tailwind changes in astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://stephanielemmeyer.me',
-  integrations: [
-    tailwind()
-  ]
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
